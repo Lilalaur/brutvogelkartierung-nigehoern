7d8aee9a5ee578b6bcdf4bcb91075f27a94780eb
Lilalaur <92028060+Lilalaur@users.noreply.github.com>
2022-01-31 22:32:05 +0100

    Info-Button

--- src/features/map/modules/controls.js
+++ src/features/map/modules/controls.js
@@ -1,12 +1,22 @@
 import { registerLegendControl } from "./legend";
 import { registerTimelineControl } from "./timeline";
 import * as L from "leaflet";
+import "leaflet.infoButton";
+import "./controls.css";
 
 /** Register all custom Leaflet controls at once */
 function registerMapControls(map) {
   registerTimelineControl(map);
   registerLegendControl(map);
   L.control.scale({ imperial: false }).addTo(map); //metrischer Maßstabsbalken
+  L.control
+    .infoButton({
+      buttonTittle: "I'm a button, press me!",
+      title: "<h1>Danke</h1>",
+      position: "topleft",
+      html: "Du bist süß.",
+    })
+    .addTo(map);
 }
 
 export { registerMapControls };
