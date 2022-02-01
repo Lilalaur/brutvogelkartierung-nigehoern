41a8f955a76ce3c7f0addb786e36c9f0e0fd9f5d
Lilalaur <92028060+Lilalaur@users.noreply.github.com>
2022-02-01 21:34:51 +0100

    Datenherkunft-Infobutton

--- src/features/map/modules/controls.js
+++ src/features/map/modules/controls.js
@@ -3,6 +3,8 @@ import { registerTimelineControl } from "./timeline";
 import * as L from "leaflet";
 import "leaflet.infoButton";
 import "./controls.css";
+import logoJordsand from "../../../assets/pics/Logo_Jordsand.png";
+import logoNPHW from "../../../assets/pics/Logo_NPHW.png";
 
 /** Register all custom Leaflet controls at once */
 function registerMapControls(map) {
@@ -11,10 +13,19 @@ function registerMapControls(map) {
   L.control.scale({ imperial: false }).addTo(map); //metrischer Maßstabsbalken
   L.control
     .infoButton({
-      buttonTittle: "I'm a button, press me!",
-      title: "<h1>Danke</h1>",
+      buttonTittle: "Datenherkunft",
+      title: "<h1>Datenherkunft</h1>",
       position: "topleft",
-      html: "Du bist süß.",
+      html: `
+      <div class="info-layer">
+        <p>Die im Rahmen dieser Applikation gezeigten Daten der Brutvogelkartierung auf Nigehörn wurden von der Nationalparkverwaltung Hamburgisches Wattenmeer bereitgestellt. 
+           Die Erfassung erfolgte durch den Verein Jordsand. Danke dafür! </p>
+        <div class="info-layer-logos">
+          <img class="logo" src="${logoNPHW}" />
+          <img class="logo Jordi" src="${logoJordsand}" />
+        </div>
+      </div>
+      `,
     })
     .addTo(map);
 }
